b3b1644c33e33dae4d7a1e4f960dd7fe12be3ba3
Rohit Jena <168836445+RohitJena1075@users.noreply.github.com>
2025-05-06 15:05:03 +0530

    Update utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -1,12 +1,10 @@
-from dotenv import load_dotenv
 import os
 
 def load_env():
-    load_dotenv()
     return {
-        # "openai_key": os.getenv("OPENAI_API_KEY"),     # Optional now
         "giphy_key": os.getenv("GIPHY_API_KEY"),
         "cohere_key": os.getenv("COHERE_API_KEY")
     }
+
 
 
